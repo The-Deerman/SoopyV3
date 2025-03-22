d99283e64edd259e3cd96d396658860779ce72a6
The_Deerman <113066400+The-Deerman@users.noreply.github.com>
2025-03-22 11:07:09 +0100

    Add: SoopyV2

---
+++ SoopyV2/features/featureBase/index.js
@@ -0,0 +1,25 @@
+
+
+import Feature from "../../featureClass/class";
+
+class FeatureBase extends Feature {
+    constructor() {
+        super();
+    }
+
+    onEnable() {
+        this.initVariables();
+    }
+
+    initVariables() {
+    }
+
+    onDisable() {
+        this.initVariables();
+    }
+}
+
+
+module.exports = {
+    class: new FeatureBase
+};
---
+++ SoopyV2/features/settings/locationSettingHolder.js
@@ -0,0 +1,28 @@
+class LocationSettingHolder {
+    constructor() {
+        this.data = new Set;
+    }
+
+    addLocationSetting(setting) {
+        this.data.add(setting);
+    }
+
+    removeLocationSetting(setting) {
+        this.data.delete(setting);
+    }
+
+    getData() {
+        return [...this.data];
+    }
+}
+
+
+if (!global.LocationSettingHolder) {
+    global.LocationSettingHolder = new LocationSettingHolder;
+
+    register("gameUnload", () => {
+        global.LocationSettingHolder = undefined;
+    });
+}
+
+export default global.LocationSettingHolder;
